96154f7c5858853b77b51be2c4395dd04e56dbe0
camillie15 <138990540+camillie15@users.noreply.github.com>
2024-08-29 17:22:56 -0500

    Use Case: Buy Cryptocurrencies from the Exchange

--- src/main/java/service/exchange/ExchangeSystemService.java
+++ src/main/java/service/exchange/ExchangeSystemService.java
@@ -2,8 +2,13 @@ package service.exchange;
 
 import model.crypto.Crypto;
 import model.crypto.TypeCrypto;
+import model.exceptions.InsufficientCryptoException;
+import model.exceptions.InsufficientFiatMoney;
+import model.order.BuyOrder;
 import model.system.ExchangeSystem;
+import service.order.BuyOrdersService;
 import service.user.UserService;
+import service.wallet.WalletService;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -11,9 +16,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class ExchangeSystemService {
+public class ExchangeSystemService implements ExchangeSystemPort{
     private final Map<String, Crypto> cryptosExchange = new HashMap<>();
     private final UserService userService;
+
+    private final BuyOrdersService buyOrdersService;
 
     private static ExchangeSystemService exchangeInstance;
 
@@ -21,6 +28,8 @@ public class ExchangeSystemService {
         this.userService = userService;
         cryptosExchange.put("Bitcoin", new Crypto(TypeCrypto.BITCOIN, new BigDecimal("100"), new BigDecimal("50000.00")));
         cryptosExchange.put("Ethereum", new Crypto(TypeCrypto.ETHEREUM, new BigDecimal("500"), new BigDecimal("3000.00")));
+
+        this.buyOrdersService = BuyOrdersService.getBuyOrdersInstance();
 
         ExchangeSystem exchange = new ExchangeSystem(getCryptosExchange(), new ArrayList<>(), new ArrayList<>());
     }
@@ -32,6 +41,25 @@ public class ExchangeSystemService {
         return exchangeInstance;
     }
 
+    @Override
+    public void buyToExchange(TypeCrypto typeCrypto, BigDecimal amountTraded, String idUser, WalletService walletService) {
+        Crypto crypto = cryptosExchange.get(typeCrypto.getNameCrypto());
+        BigDecimal totalPay = crypto.getPriceCrypto().multiply(amountTraded);
+        if (crypto.getAmountCrypto().compareTo(amountTraded) >= 0){
+            if (walletService.getWallet().getBalanceCash().compareTo(totalPay) >= 0) {
+                BuyOrder buyOrder = buyOrdersService.createBuyOrder(idUser, crypto.getTypeCrypto(), amountTraded, totalPay);
+                walletService.buyCrypto(buyOrder);
+
+                crypto.sellCrypto(amountTraded);
+            } else {
+                throw new InsufficientFiatMoney();
+            }
+        } else {
+            throw new InsufficientCryptoException();
+        }
+    }
+
+    @Override
     public List<Crypto> getCryptosExchange() {
         return new ArrayList<>(cryptosExchange.values());
     }
