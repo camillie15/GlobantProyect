7677d2e24e04df3cf89696808e7e7af54a7b89d1
camillie15 <138990540+camillie15@users.noreply.github.com>
2024-08-29 16:29:36 -0500

    Use Case: View Wallet Balance

---
+++ src/main/java/service/crypto/CryptoService.java
@@ -0,0 +1,25 @@
+package service.crypto;
+
+import model.crypto.Crypto;
+import model.crypto.TypeCrypto;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CryptoService {
+    private final Map<String, Crypto> cryptos = new HashMap<>();
+
+    public CryptoService() {
+        cryptos.put("BTC", new Crypto(TypeCrypto.BITCOIN, BigDecimal.ZERO));
+        cryptos.put("ETH", new Crypto(TypeCrypto.ETHEREUM, BigDecimal.ZERO));
+    }
+
+    public List<Crypto> getCryptos(){
+        return new ArrayList<>(cryptos.values());
+    }
+
+
+}
